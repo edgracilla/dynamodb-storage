a57ce35d04a1595673c109e19418c7a6794a42a3
Rozz <rbanquerigo@reekoh.com>
2015-11-25 16:08:06 +0800

    REEKOH-274 Storage Plugin - AWS DynamoDB Database Storage

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
-# dynamodb-storage
-AWS DynamoDB Storage Plugin for the Reekoh IoT Platform. Integrates a Reekoh instance with AWS DynamoDB instance to store device data.
+# DynamoDB Storage
+
+[![Build Status](https://travis-ci.org/Reekoh/mysql-storage.svg)](https://travis-ci.org/Reekoh/dynamodb-storage)
+![Dependencies](https://img.shields.io/david/Reekoh/dynamodb-storage.svg)
+![Dependencies](https://img.shields.io/david/dev/Reekoh/dynamodb-storage.svg)
+![Built With](https://img.shields.io/badge/built%20with-gulp-red.svg)
+
+DynamoDB Storage Plugin for the Reekoh IoT Platform.
+
+## Assumptions:
+
+1. Data would be in JSON format
+2. Data would be processed based on configuration format
+3. No data conversion within the plugin please use converters in Reekoh
+4. Field configuration is not required and will insert data as is if not specified
+
+## Process
+
+1. Data would be written directly to the DynamoDB Table in the Region it is defined
+2. Data will be written using aws-sdk standard library
+3. All errors will be logged and no data should be written
+4. Data will be parsed accordingly based on field configuration
+5. Data will be overwritten if the primary key/primary+sort key is already existing per behavior of DynamoDB
+
+## Field Configuration (Optional)
+
+1. Input for this field is in JSON format {"(field_name)" : {"source_field" : "value"}}
+2. field_name will be the name of the column in the DynamoDB Table
+3  source_field (required) value will be the name of the field in the JSON Data passed to the plugin
+
+
+```javascript
+{
+  "partition_string_pk": {
+	"source_field": "partition_string"
+  },
+  "sort_number_pk": {
+  	"source_field": "sort_number"
+  },
+  "co2_field": {
+	"source_field": "co2"
+  },
+  "temp_field": {
+	"source_field": "temp"
+  },
+  "quality_field": {
+	"source_field": "quality"
+  },
+  "metadata_field": {
+	"source_field": "metadata"
+  },
+  "reading_time_field": {
+	"source_field": "reading_time"
+  },
+  "random_data_field": {
+	"source_field": "random_data"
+  },
+  "is_normal_field": {
+	"source_field": "is_normal"
+  }
+}
+```
+
+### Sample Data:
+
+```javascript
+{
+  partition_string_pk: 'SAMPLE_PK',
+  sort_number_pk: 123456
+  co2: '11%',
+  temp: 23,
+  quality: 11.25,
+  metadata: '{"name": "warehouse air conditioning"}',
+  reading_time: '2015-11-27T11:04:13.539Z',
+  random_data: 'abcdefg',
+  is_normal: true
+}
+```
+
+### DynamoDB Table Definition:
+
+Type Field mapping    |
+----------------------|
+partition_string_pk   | (Primary Key - string)
+sort_number_pk        | (Sort Key - number)
+
---
+++ app.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var platform = require('./platform'),
+	AWS     = require('aws-sdk'),
+	async   = require('async'),
+	isEmpty = require('lodash.isempty'),
+	docClient, params = {};
+
+
+/**
+ * Emitted when device data is received. This is the event to listen to in order to get real-time data feed from the connected devices.
+ * @param {object} data The data coming from the device represented as JSON Object.
+ */
+platform.on('data', function (data) {
+	// TODO: Insert the data to the database using the initialized connection.
+	var processedData = {};
+
+
+	var save = function() {
+		var itemParams  = {TableName : params.table, Item : {}};
+		itemParams.Item = processedData;
+
+		docClient.put(itemParams, function(error, resp) {
+			if (error) {
+				console.error('Error creating record on Elasticsearch', error);
+				platform.handleException(error);
+			} else {
+				platform.log(JSON.stringify({
+					title: 'Record Successfully inserted to DynamoDB.',
+					data: processedData
+				}));
+			}
+		});
+	};
+
+	if (params.fields) {
+		async.forEachOf(params.fields, function(field, key, callback) {
+			var datum = data[field.source_field];
+			if (datum !== undefined && datum !== null) processedData[key] = datum;
+			callback();
+		}, save);
+	} else {
+		processedData = data;
+		save();
+	}
+
+});
+
+/**
+ * Emitted when the platform shuts down the plugin. The Storage should perform cleanup of the resources on this event.
+ */
+platform.once('close', function () {
+	var domain = require('domain');
+	var d = domain.create();
+
+	d.once('error', function(error) {
+		console.error(error);
+		platform.handleException(error);
+		platform.notifyClose();
+		d.exit();
+	});
+
+	d.run(function() {
+		platform.notifyClose(); // Notify the platform that resources have been released.
+		d.exit();
+	});
+});
+
+/**
+ * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
+ * Afterwards, platform.notifyReady() should be called to notify the platform that the init process is done.
+ * @param {object} options The options or configuration injected by the platform to the plugin.
+ */
+platform.once('ready', function (options) {
+
+	var init = function(e) {
+
+		if (e) {
+			console.error('Error parsing JSON field configuration for DynamoDB.', e);
+			return platform.handleException(e);
+		}
+
+		AWS.config.update({
+			accessKeyId     : options.accessKeyId,
+			secretAccessKey : options.secretAccessKey
+		});
+
+		docClient = new AWS.DynamoDB.DocumentClient({region: options.region});
+		params.table = options.table;
+
+		platform.log('DynamoDB plugin ready.');
+		platform.notifyReady();
+
+	};
+
+	if (options.fields) {
+		var parseFields = JSON.parse(options.fields);
+		params.fields = parseFields;
+
+		async.forEachOf(parseFields , function(field, key, callback) {
+
+			if (isEmpty(field.source_field)){
+				callback( new Error('Source field is missing for ' + field + ' in DynamoDB Plugin'));
+			} else
+				callback();
+		}, init);
+
+	} else {
+		init(null);
+	}
+
+});
